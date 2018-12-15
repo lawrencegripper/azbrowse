a6a80d87c710ca4c4ceef98dc0e7ad095e9669a2
Lawrence Gripper <info@grippers.co.uk>
2018-12-15 07:39:22 +0000

    Fix selupdate issue

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 // Overridden via ldflags
 var (
 	BuildDataPlatform  = "unknown"
-	BuildDataVersion   = "devbuild"
+	BuildDataVersion   = "0.0.1"
 	BuildDataGitCommit = "unknown"
 	BuildDataBuildDate = "unknown"
 	BuildDataGoVersion = "unknown"
