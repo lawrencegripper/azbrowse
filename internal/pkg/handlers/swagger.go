14de04b9dfcd33a547e836c888c4a1c742e1259d
Stuart Leeks <stuartle@microsoft.com>
2019-02-21 21:34:39 +0000

    Cache swagger resource type

--- internal/pkg/handlers/swagger.go
+++ internal/pkg/handlers/swagger.go
@@ -42,16 +42,21 @@ func mustGetEndpointInfoFromURL(url string, apiVersion string) endpoints.Endpoin
 	return endpoint
 }
 
-func getResourceTypeForURL(url string, resourceTypes []ResourceType) *ResourceType {
+func getResourceTypeForURL(ctx context.Context, url string, resourceTypes []ResourceType) *ResourceType {
+	span, ctx := tracing.StartSpanFromContext(ctx, "getResourceTypeForURL:"+url)
+	defer span.Finish()
+	return getResourceTypeForURLInner(url, resourceTypes)
+}
+func getResourceTypeForURLInner(url string, resourceTypes []ResourceType) *ResourceType {
 	for _, resourceType := range resourceTypes {
 		matchResult := resourceType.Endpoint.Match(url)
 		if matchResult.IsMatch {
 			return &resourceType
 		}
-		if result := getResourceTypeForURL(url, resourceType.SubResources); result != nil {
+		if result := getResourceTypeForURLInner(url, resourceType.SubResources); result != nil {
 			return result
 		}
-		if result := getResourceTypeForURL(url, resourceType.Children); result != nil {
+		if result := getResourceTypeForURLInner(url, resourceType.Children); result != nil {
 			return result
 		}
 	}
@@ -68,8 +73,12 @@ func (e *SwaggerResourceExpander) ensureInitialized() {
 func (e *SwaggerResourceExpander) DoesExpand(ctx context.Context, currentItem *TreeNode) (bool, error) {
 	e.ensureInitialized()
 	if currentItem.ItemType == resourceType {
-		item := getResourceTypeForURL(currentItem.ExpandURL, e.ResourceTypes)
-		if item != nil {
+		if currentItem.SwaggerResourceType != nil {
+			return true, nil
+		}
+		resourceType := getResourceTypeForURL(ctx, currentItem.ExpandURL, e.ResourceTypes)
+		if resourceType != nil {
+			currentItem.SwaggerResourceType = resourceType // cache to avoid looking up in Expand
 			return true, nil
 		}
 	}
@@ -80,12 +89,12 @@ func (e *SwaggerResourceExpander) DoesExpand(ctx context.Context, currentItem *T
 // Expand returns Resources in the RG
 func (e *SwaggerResourceExpander) Expand(ctx context.Context, currentItem *TreeNode) ExpanderResult {
 
-	span, ctx := tracing.StartSpanFromContext(ctx, "expand:"+currentItem.ItemType+":"+currentItem.Name+":"+currentItem.ID, tracing.SetTag("item", currentItem))
+	span, ctx := tracing.StartSpanFromContext(ctx, "expand(swagger):"+currentItem.ItemType+":"+currentItem.Name+":"+currentItem.ID, tracing.SetTag("item", currentItem))
 	defer span.Finish()
 
-	resourceType := getResourceTypeForURL(currentItem.ExpandURL, e.ResourceTypes)
+	resourceType := currentItem.SwaggerResourceType
 	if resourceType == nil {
-		panic(fmt.Errorf("Node Item not found"))
+		panic(fmt.Errorf("SwaggerResourceType not set"))
 	}
 
 	method := resourceType.Verb
@@ -111,23 +120,27 @@ func (e *SwaggerResourceExpander) Expand(ctx context.Context, currentItem *TreeN
 			panic(err)
 		}
 		for _, resource := range resourceResponse.Resources {
-			subResourceType := getResourceTypeForURL(resource.ID, resourceType.SubResources)
+			subResourceType := getResourceTypeForURL(ctx, resource.ID, resourceType.SubResources)
+			if subResourceType == nil {
+				panic(fmt.Errorf("SubResource type not found! %s", resource.ID))
+			}
 			subResourceTemplateValues := subResourceType.Endpoint.Match(resource.ID).Values
 			name := substituteValues(subResourceType.Display, subResourceTemplateValues)
 			newItems = append(newItems, &TreeNode{
-				Parentid:  currentItem.ID,
-				Namespace: "swagger",
-				Name:      name,
-				Display:   name,
-				ExpandURL: resource.ID + "?api-version=" + subResourceType.Endpoint.APIVersion,
-				ItemType:  "resource",
-				DeleteURL: "TODO",
+				Parentid:            currentItem.ID,
+				Namespace:           "swagger",
+				Name:                name,
+				Display:             name,
+				ExpandURL:           resource.ID + "?api-version=" + subResourceType.Endpoint.APIVersion,
+				ItemType:            "resource",
+				DeleteURL:           "TODO",
+				SwaggerResourceType: subResourceType,
 			})
 		}
 	}
 	// Add any children to newItems
 	for _, child := range resourceType.Children {
-
+		loopChild := child
 		url, err := child.Endpoint.BuildURL(templateValues)
 		if err != nil {
 			err = fmt.Errorf("Error building URL: %s\nURL:%s", child.Display, err)
@@ -135,13 +148,14 @@ func (e *SwaggerResourceExpander) Expand(ctx context.Context, currentItem *TreeN
 		}
 		display := substituteValues(child.Display, templateValues)
 		newItems = append(newItems, &TreeNode{
-			Parentid:  currentItem.ID,
-			Namespace: "swagger",
-			Name:      display,
-			Display:   display,
-			ExpandURL: url,
-			ItemType:  "resource",
-			DeleteURL: "NotSupported",
+			Parentid:            currentItem.ID,
+			Namespace:           "swagger",
+			Name:                display,
+			Display:             display,
+			ExpandURL:           url,
+			ItemType:            "resource",
+			DeleteURL:           "NotSupported",
+			SwaggerResourceType: &loopChild,
 		})
 	}
 
