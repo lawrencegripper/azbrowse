52a8e30c09fc53f3a7f99bca212a44e6f7d61510
Martin Peck <mpeck@hotmail.co.uk>
2019-02-22 17:01:19 +0000

    restore all of the trailing whitespace

--- cmd/azbrowse/help.go
+++ cmd/azbrowse/help.go
@@ -17,42 +17,42 @@ func ToggleHelpView(g *gocui.Gui) {
 func DrawHelp(v *gocui.View) {
 
 	fmt.Fprint(v, style.Header(`
-	--> PRESS CTRL+I TO CLOSE THIS AND CONTINUE. YOU CAN OPEN IT AGAIN WITH CRTL+I AT ANY TIME. <--
-                             _       ___
-                            /_\   __| _ )_ _ _____ __ _____ ___
-                           / _ \ |_ / _ \ '_/ _ \ V  V (_-</ -_)
-                          /_/ \_\/__|___/_| \___/\_/\_//__/\___|
-                        Interactive CLI for browsing Azure resources
-# Navigation
-
-| Key         | Does                 |
-| ----------- | -------------------- |
-| ⇧ / ⇩       | Select resource      |
-| ⇦ / ⇨       | Select Menu/JSON     |
-| Backspace   | Go back              |
-| ENTER       | Expand/View resource |
-| F5          | Refresh              |
-| CTRL+I      | Show this page       |
-
-# Operations
-
-| Key                 | Does                      |                                                                                    |
-| ------------------- | ------------------------- | ---------------------------------------------------------------------------------- |
-| CTRL+E              | Toggle Browse JSON        | For longer responses you can move the cursor to scroll the doc                     |
-| CTLT+F              | Toggle Fullscreen         | Gives a fullscreen view of the JSON for smaller terminals                          |
-| CTRL+O (o for open) | Open Portal               | Opens the portal at the currently selected resource                                |
-| DEL                 | Delete resource           | The currently selected resource will be deleted (Requires double press to confirm) |
-| CTLT+S              | Save JSON to clipboard    | Saves the last JSON response to the clipboard for export                           |
-| CTLT+A              | View Actions for resource | This allows things like ListKeys on storage or Restart on VMs                      |
-
-For bugs, issue or to contribute visit: https://github.com/lawrencegripper/azbrowse
-
-# Status Icons
-
-Deleting: ☠ Failed: ⛈  Updating: ⟳  Resuming/Starting:    ⛅  Provisioning: ⌛
-Creating\Preparing: 🏗  Scaling:  ⚖   Suspended/Suspending: ⛔  Succeeded:   ☼
-
---> PRESS CTRL+I TO CLOSE THIS AND CONTINUE. YOU CAN OPEN IT AGAIN WITH CRTL+I AT ANY TIME. <--
+	--> PRESS CTRL+I TO CLOSE THIS AND CONTINUE. YOU CAN OPEN IT AGAIN WITH CRTL+I AT ANY TIME. <--                                                                                                                                  
+                             _       ___                                                                                                                                                                                                 
+                            /_\   __| _ )_ _ _____ __ _____ ___                                                                                                                                                                          
+                           / _ \ |_ / _ \ '_/ _ \ V  V (_-</ -_)                                                                                                                                                                         
+                          /_/ \_\/__|___/_| \___/\_/\_//__/\___|                                                                                                                                                                         
+                        Interactive CLI for browsing Azure resources                                                                                                                                                                     
+# Navigation                                                                                                                                                                                                                                                
+                                                                                                                                                                                                                                                
+| Key         | Does                 |                                                                                                                                                                                                                                              
+| ----------- | -------------------- |                                                                                                                                                                                                                                              
+| ⇧ / ⇩       | Select resource      |                                                                                                                                                                                                                                              
+| ⇦ / ⇨       | Select Menu/JSON     |                                                                                                                                                                                                                                                             
+| Backspace   | Go back              |                                                                                                                                                                                                           
+| ENTER       | Expand/View resource |                                                                                                                                                                                                           
+| F5          | Refresh              |                                                                                                                                                                                                           
+| CTRL+I      | Show this page       |                                                                                                                                                                                                           
+                                                                                                                                                                                                           
+# Operations	                                                                                                                                                                                                           
+                                                                                                                                                                                                           
+| Key                 | Does                      |                                                                                    |                                                                                                                                                                                                           
+| ------------------- | ------------------------- | ---------------------------------------------------------------------------------- |                                                                                                                                                                                                           
+| CTRL+E              | Toggle Browse JSON        | For longer responses you can move the cursor to scroll the doc                     |                                                                                                                                                                                                           
+| CTLT+F              | Toggle Fullscreen         | Gives a fullscreen view of the JSON for smaller terminals                          |                                                                                                                                                                                                           
+| CTRL+O (o for open) | Open Portal               | Opens the portal at the currently selected resource                                |                                                                                                                                                                                                           
+| DEL                 | Delete resource           | The currently selected resource will be deleted (Requires double press to confirm) |                                                                                                                                                                                                           
+| CTLT+S              | Save JSON to clipboard    | Saves the last JSON response to the clipboard for export                           |                                                                                                                                                                                                           
+| CTLT+A              | View Actions for resource | This allows things like ListKeys on storage or Restart on VMs                      |                                                                                                                                                                                                           
+                                                                                                                                                                                                           
+For bugs, issue or to contribute visit: https://github.com/lawrencegripper/azbrowse                                                                                                                                                                                                                                 
+                                                                                                                                                                                                           
+# Status Icons                                                                                                                                                                                                           
+                                                                                                                                                                                                           
+Deleting: ☠ Failed: ⛈  Updating: ⟳  Resuming/Starting:    ⛅  Provisioning: ⌛                                                                                                                                                                                                            
+Creating\Preparing: 🏗  Scaling:  ⚖   Suspended/Suspending: ⛔  Succeeded:   ☼                                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                                                                                                                            
+--> PRESS CTRL+I TO CLOSE THIS AND CONTINUE. YOU CAN OPEN IT AGAIN WITH CRTL+I AT ANY TIME. <--                                                                                                                                                                                                            
 
 `))
 
