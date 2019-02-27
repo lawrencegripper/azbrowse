9cd60cdd0fe5fc1a93f2931a9a732bf7f25e3ab0
Lawrence Gripper <info@grippers.co.uk>
2019-02-27 13:34:30 +0000

    Fix docker login issue.

--- scripts/circleci/should_publish.sh
+++ scripts/circleci/should_publish.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ -z $CIRCLE_PR_NUMBER ] && [[ $CIRCLE_BRANCH == "master" ]]; then
     echo 'export PUBLISH=true' >> $BASH_ENV
+    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     echo "On master setting PUBLISH=true"
 else 
     echo "Skipping publish as is from PR: $CIRCLE_PR_NUMBER or not master BRANCH: $CIRCLE_BRANCH"
