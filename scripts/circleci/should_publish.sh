2b47d1c27d6c39c77173c3dde32857f1143c26ab
Lawrence Gripper <info@grippers.co.uk>
2019-02-27 13:10:46 +0000

    Fix circle should build.

--- scripts/circleci/should_publish.sh
+++ scripts/circleci/should_publish.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ -z $CIRCLE_PR_NUMBER ] && [[ $CIRCLE_BRANCH == "master" ]]; then
-    'export PUBLISH=true' >> $BASH_ENV
+    echo 'export PUBLISH=true' >> $BASH_ENV
     echo "On master setting PUBLISH=true"
 else 
     echo "Skipping publish as is from PR: $CIRCLE_PR_NUMBER or not master BRANCH: $CIRCLE_BRANCH"
