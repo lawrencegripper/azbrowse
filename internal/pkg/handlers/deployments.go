5d404b65263fcc9a2918755a4e88e378bec9370d
Lawrence Gripper <info@grippers.co.uk>
2019-03-18 17:39:33 +0000

    Update display formatting for deployment operations

--- internal/pkg/handlers/deployments.go
+++ internal/pkg/handlers/deployments.go
@@ -86,10 +86,14 @@ func (e *DeploymentsExpander) Expand(ctx context.Context, currentItem *TreeNode)
 			// Update the existing state as we have more up-to-date info
 			objectJSON := string(value.GetArray("value")[i].MarshalTo([]byte("")))
 
-			display := operation.OperationID + "\n   " + style.Subtle("Started:"+operation.Properties.Timestamp) + "\n   " + style.Subtle("Duration: "+operation.Properties.Duration) + "\n   " + style.Subtle("DeploymentStatus: "+operation.Properties.ProvisioningState+"")
+			title := operation.OperationID
 			if operation.Properties.TargetResource.ResourceType != "" {
-				display += "\n   " + style.Subtle("ResourceType:"+operation.Properties.TargetResource.ResourceType) +
-					"\n   " + style.Subtle("ResourceName:"+operation.Properties.TargetResource.ResourceName)
+				title = operation.Properties.TargetResource.ResourceName
+			}
+
+			display := title + "\n   " + style.Subtle("Started:"+operation.Properties.Timestamp) + "\n   " + style.Subtle("Duration: "+operation.Properties.Duration) + "\n   " + style.Subtle("DeploymentStatus: "+operation.Properties.ProvisioningState+"")
+			if operation.Properties.TargetResource.ResourceType != "" {
+				display += "\n   " + style.Subtle("ResourceType:"+operation.Properties.TargetResource.ResourceType)
 			}
 			newItems = append(newItems, &TreeNode{
 				Name:           operation.OperationID,
