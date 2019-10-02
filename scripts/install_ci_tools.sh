86b776869f23ada666166baefdafeb9f6c9376f6
Lawrence Gripper <info@grippers.co.uk>
2019-10-02 12:01:19 +0100

    Fix circleCi Golangci-linter issue.

--- scripts/install_ci_tools.sh
+++ scripts/install_ci_tools.sh
@@ -2,4 +2,4 @@
 set -e
 
 # Download and install golangci-lint (https://github.com/golangci/golangci-lint)
-curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin
+curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sed 's/tar -/tar --no-same-owner -/g' | sh -s -- -b $(go env GOPATH)/bin
