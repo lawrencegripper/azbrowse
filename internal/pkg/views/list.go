f26a2e47cba12e21d9ffa4f52f6e0c7b8bc0ce42
Stuart Leeks <stuartle@microsoft.com>
2019-10-11 17:05:56 +0100

    Fix remembering selected item on back nav
    Also fixes refresh

--- internal/pkg/views/list.go
+++ internal/pkg/views/list.go
@@ -347,8 +347,7 @@ func (w *ListWidget) Navigate(nodes []*handlers.TreeNode, content string, title 
 
 // SetNodes allows others to set the list nodes
 func (w *ListWidget) SetNodes(nodes []*handlers.TreeNode) {
-	w.selected = 0
-
+	
 	// Capture current view to navstack
 	if w.HasCurrentItem() {
 		w.navStack.Push(&Page{
@@ -358,7 +357,7 @@ func (w *ListWidget) SetNodes(nodes []*handlers.TreeNode) {
 			Selection:        w.selected,
 			ExpandedNodeItem: w.CurrentItem(),
 		})
-
+		
 		currentID := w.CurrentItem().ID
 		for _, node := range nodes {
 			if node.ID == currentID {
@@ -366,7 +365,8 @@ func (w *ListWidget) SetNodes(nodes []*handlers.TreeNode) {
 			}
 		}
 	}
-
+	
+	w.selected = 0
 	w.items = nodes
 	w.ClearFilter()
 }
