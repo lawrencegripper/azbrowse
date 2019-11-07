f11b0108deaf1174e37d9d217d004efbf512f077
jjcollinge <jonathancollinge@live.com>
2019-11-07 01:44:05 +0000

    fixed demo regex

--- internal/pkg/views/demomode.go
+++ internal/pkg/views/demomode.go
@@ -8,17 +8,20 @@ func stripSecretVals(s string) string {
 	guidRegex := regexp.MustCompile(`[{(]?[0-9a-f]{8}[-]?([0-9a-f]{4}[-]?){3}[0-9a-f]{12}[)}]?`)
 	s = guidRegex.ReplaceAllString(s, "00000000-0000-0000-0000-HIDDEN000000")
 
-	idRegex := regexp.MustCompile(`"id":.*`)
-	s = idRegex.ReplaceAllString(s, `"id": "HIDDEN",`)
+	idRegex := regexp.MustCompile(`"id":\s*".+?(?:\\"|[^"])*"`)
+	s = idRegex.ReplaceAllString(s, `"id": "HIDDEN"`)
 
-	locationRegex := regexp.MustCompile(`"location":.*`)
-	s = locationRegex.ReplaceAllString(s, `"location": "HIDDEN-LOCATION",`)
+	managedByRegex := regexp.MustCompile(`"managedBy":\s*".+?(?:\\"|[^"])*"`)
+	s = managedByRegex.ReplaceAllString(s, `"managedBy": "HIDDEN_MANAGED_BY"`)
 
-	fqdnRegex := regexp.MustCompile(`"fqdn":.*`)
-	s = fqdnRegex.ReplaceAllString(s, `"fqdn": "HIDDEN-FQDN",`)
+	locationRegex := regexp.MustCompile(`"location":\s*".+?(?:\\"|[^"])*"`)
+	s = locationRegex.ReplaceAllString(s, `"location": "HIDDEN-LOCATION"`)
 
-	nameRegex := regexp.MustCompile(`"name":.*`)
-	s = nameRegex.ReplaceAllString(s, `"name": "HIDDEN-NAME",`)
+	fqdnRegex := regexp.MustCompile(`"fqdn":\s*".+?(?:\\"|[^"])*"`)
+	s = fqdnRegex.ReplaceAllString(s, `"fqdn": "HIDDEN-FQDN"`)
+
+	nameRegex := regexp.MustCompile(`"name":\s*".+?(?:\\"|[^"])*"`)
+	s = nameRegex.ReplaceAllString(s, `"name": "HIDDEN-NAME"`)
 
 	storageKeyRegex := regexp.MustCompile(`([A-Za-z/+0-9]{86})(==)`)
 	s = storageKeyRegex.ReplaceAllString(s, "HIDDEN-KEY")
