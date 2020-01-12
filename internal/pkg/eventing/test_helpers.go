0240b1640d98e40dff659dc65d818f18c98bdfe1
Lawrence Gripper <info@grippers.co.uk>
2020-01-12 22:08:50 +0000

    Fix test broken by eventing refactoring

--- internal/pkg/eventing/test_helpers.go
+++ internal/pkg/eventing/test_helpers.go
@@ -6,23 +6,23 @@ import (
 )
 
 // WaitForCompletedStatusEvent waits for a completed event
-func WaitForCompletedStatusEvent(t *testing.T, statusEvents chan interface{}, waitForSec int) StatusEvent {
+func WaitForCompletedStatusEvent(t *testing.T, statusEvents chan interface{}, waitForSec int) *StatusEvent {
 	return WaitForStatusEvent(t, statusEvents, waitForSec, false)
 }
 
 // WaitForFailureStatusEvent waits for failure event
-func WaitForFailureStatusEvent(t *testing.T, statusEvents chan interface{}, waitForSec int) StatusEvent {
+func WaitForFailureStatusEvent(t *testing.T, statusEvents chan interface{}, waitForSec int) *StatusEvent {
 	return WaitForStatusEvent(t, statusEvents, waitForSec, true)
 }
 
 // WaitForStatusEvent lets you wait on a event
-func WaitForStatusEvent(t *testing.T, statusEvents chan interface{}, waitForSec int, expectError bool) StatusEvent {
+func WaitForStatusEvent(t *testing.T, statusEvents chan interface{}, waitForSec int, expectError bool) *StatusEvent {
 	for index := 0; index < waitForSec; index++ {
 		select {
 		case <-time.After(time.Second):
 			t.Log("Waited 1 sec...")
 		case statusRaw := <-statusEvents:
-			statusEvent := statusRaw.(StatusEvent)
+			statusEvent := statusRaw.(*StatusEvent)
 			t.Logf("EVENT STATUS MESSAGE: %s Failure: %v InProgress: %v", statusEvent.Message, statusEvent.Failure, statusEvent.InProgress)
 			// Wait for things to finish
 			if statusEvent.Failure && !expectError {
@@ -39,5 +39,5 @@ func WaitForStatusEvent(t *testing.T, statusEvents chan interface{}, waitForSec 
 	}
 
 	t.Error("Waited for event which never occurred")
-	return StatusEvent{}
+	return &StatusEvent{}
 }
