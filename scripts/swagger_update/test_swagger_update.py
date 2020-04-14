28b280a5c67636b51ea1fef05e8b474436da137f
Stuart Leeks <stuartle@microsoft.com>
2020-04-14 12:56:09 +0100

    Fix up swagger update tests after refactoring

--- scripts/swagger_update/test_swagger_update.py
+++ scripts/swagger_update/test_swagger_update.py
@@ -3,15 +3,21 @@ from api_set import *
 
 
 def test_get_api_version_from_readme_with_invalid_file_path_returns_none():
+    dummy_overrides = {}
     result = get_api_version_from_readme(
-        "./scripts/swagger_update/test_data/does_not_exist.md"
+        "dummy",
+        "./scripts/swagger_update/test_data/does_not_exist.md",
+        dummy_overrides
     )
     assert result == None, "Should return None for file that doesn't exist"
 
 
 def test_get_api_version_from_readme_with_simple_tags_returns_correct_fileset():
+    dummy_overrides = {}
     api_version = get_api_version_from_readme(
-        "./scripts/swagger_update/test_data/file_with_simple_tags.md"
+        "dummy",
+        "./scripts/swagger_update/test_data/file_with_simple_tags.md",
+        dummy_overrides
     )
 
     assert api_version != None, "Expected api_version"
@@ -34,8 +40,11 @@ def test_get_api_version_from_readme_with_simple_tags_returns_correct_fileset():
 
 
 def test_get_api_version_from_readme_with_multiple_tags_on_a_line_returns_correct_fileset():
+    dummy_overrides = {}
     api_version = get_api_version_from_readme(
-        "./scripts/swagger_update/test_data/file_with_multiple_tags_per_line.md"
+        "dummy",
+        "./scripts/swagger_update/test_data/file_with_multiple_tags_per_line.md",
+        dummy_overrides
     )
 
     assert api_version != None, "Expected api_version"
