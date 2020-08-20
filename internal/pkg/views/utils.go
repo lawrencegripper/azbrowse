5853c5cc395101875cc9442320121b6695740888
Stuart Leeks <stuartle@microsoft.com>
2020-08-20 19:14:42 +0100

    Avoid panic when resizing to narrow terminal

--- internal/pkg/views/utils.go
+++ internal/pkg/views/utils.go
@@ -24,5 +24,15 @@ func getViewBounds(g *gocui.Gui, x int, y int, width int, height int) (x0, y0, x
 	} else {
 		y1 = y0 + height // fixed height
 	}
+
+	if x1 > sx {
+		x1 = sx // don't allow x1 to exceed terminal width
+	}
+	if x1 <= x0 {
+		x0 = x1 - 2 // ensure we don't end up with zero width view
+	}
+	if y1 <= y0 {
+		y0 = y1 - 2 // ensure we don't end up with zero height view
+	}
 	return
 }
